7b51c0468df001dc88719a6d76cebd9b8ac3ee01
Ankur Sethi <contact@ankursethi.in>
2020-09-24 18:37:33 +0530

    Add markdown-mode

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -42,4 +42,13 @@
   :config
   (add-hook 'js-mode-hook #'smartparens-mode))
 
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
+
 (provide 'init-prog)
