5bd1ee2f1bddd929ceff6dddf1547383d289c9d9
Ankur Sethi <contact@ankursethi.in>
2020-09-27 15:23:47 +0530

    Enable global-auto-revert mode correctly

--- lisp/init-files.el
+++ lisp/init-files.el
@@ -1,7 +1,7 @@
 (setq-default create-lockfiles nil)
 (setq-default make-backup-files nil)
 
-(global-auto-revert-mode)
+(global-auto-revert-mode 1)
 (setq dired-auto-revert-buffer t)
 
 (provide 'init-files)
