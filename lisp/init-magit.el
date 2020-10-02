487cd487e189a66b4bebdcb604f632af8b952639
Ankur Sethi <contact@ankursethi.in>
2020-10-02 12:16:20 +0530

    Enable Magit Forge

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -1,6 +1,12 @@
 (use-package magit
   :ensure t)
 
+(setq auth-sources '("~/.authinfo"))
+
+(use-package forge
+  :ensure t
+  :after magit)
+
 (global-set-key (kbd "C-x g") 'magit-status)
 
 (provide 'init-magit)
