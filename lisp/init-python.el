2c98c50450e801a1caba99a9b22657610f9151b8
Ankur Sethi <contact@ankursethi.in>
2020-10-08 11:24:24 +0530

    Format Python code on save

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,8 +1,13 @@
 (use-package elpy
   :ensure t
+  :after flycheck
   :config
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode)
+  (add-hook 'elpy-mode-hook (lambda ()
+                              (add-hook 'before-save-hook
+                                        'elpy-black-fix-code nil t)))
+
   (elpy-enable))
 
 (provide 'init-python)
