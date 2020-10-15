ef230f6a0dbbc212fb85f949f54d1fafd3c778f0
Ankur Sethi <contact@ankursethi.in>
2020-10-15 19:54:49 +0530

    Remove smart move to start function, configure smartparens

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -1,31 +1,3 @@
-(defun s3thi/smart-move-beginning-of-line (arg)
-  "Move point back to indentation of beginning of line.
-
-Move point to the first non-whitespace character on this line.
-If point is already there, move to the beginning of the line.
-Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-
-If ARG is not nil or 1, move forward ARG - 1 lines first.  If
-point reaches the beginning or end of the buffer, stop there.
-
-Source: https://emacsredux.com/blog/2013/05/22/smarter-navigation-to-the-beginning-of-a-line/"
-  (interactive "^p")
-  (setq arg (or arg 1))
-
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
-
-  (let ((orig-point (point)))
-    (back-to-indentation)
-    (when (= orig-point (point))
-      (move-beginning-of-line 1))))
-
-(global-set-key [remap move-beginning-of-line]
-                #'s3thi/smart-move-beginning-of-line)
-
-
 (setq-default indent-tabs-mode nil)
 
 (use-package expand-region
@@ -37,6 +9,9 @@ Source: https://emacsredux.com/blog/2013/05/22/smarter-navigation-to-the-beginni
   :ensure t
   :diminish
   :config
+  (require 'smartparens-config)
+  (sp-with-modes '(js-mode)
+    (sp-local-pair "{" nil :post-handlers '(("||\n[i]" "RET"))))
   (add-hook 'prog-mode-hook #'smartparens-mode))
 
 (setq save-interprogram-paste-before-kill t)
