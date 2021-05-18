ba890b8f28daa61f57bc2a77823eafd6e0cd3eaf
Ankur Sethi <contact@ankursethi.in>
2021-05-18 18:26:34 +0530

    Change magit binding and remove git-gutter

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -7,19 +7,4 @@
   :ensure t
   :after magit)
 
-(global-set-key (kbd "s-m") 'magit-status)
-
-(use-package git-gutter
-  :ensure t
-  :diminish
-  :config
-  (global-git-gutter-mode +1))
-
-(global-set-key (kbd "C-x C-g") 'git-gutter)
-(global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
-(global-set-key (kbd "C-,") 'git-gutter:previous-hunk)
-(global-set-key (kbd "C-.") 'git-gutter:next-hunk)
-(global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
-(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
-
 (provide 'init-git)
