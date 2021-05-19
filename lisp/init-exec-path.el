32bb1162eb73e05fc40a21e7edf7cdaf9c4fe307
Ankur Sethi <contact@ankursethi.in>
2021-05-19 10:07:04 +0530

    Only load exec-path-from-shell on macOS

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -1,9 +1,10 @@
-(use-package exec-path-from-shell
-  :ensure t
-  :config
-  (when (memq window-system '(mac ns x))
-    (exec-path-from-shell-initialize))
-  (exec-path-from-shell-copy-env "WORKON_HOME"))
+(when *is-a-mac*
+  (use-package exec-path-from-shell
+    :ensure t
+    :config
+    (when (memq window-system '(mac ns x))
+      (exec-path-from-shell-initialize))
+    (exec-path-from-shell-copy-env "WORKON_HOME")))
 
 (use-package add-node-modules-path
   :ensure t
