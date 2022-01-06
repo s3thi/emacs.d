3e41b570811d854123a8472936f68b8a834c8bff
Ankur Sethi <contact@ankursethi.in>
2022-01-06 11:23:05 +0530

    Tweak more things

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; init.el --- Initialization file for Emacs
+;; init.el --- Initialization file for Emacs -*- lexical-binding: t -*-
 
 ;;; Commentary:
 ;; Initialization file for Emacs.
@@ -14,7 +14,8 @@
 
 ;; Load my personal blog helpers.
 (require 'alive-and-well)
-(global-set-key (kbd "C-c w") #'aaw/new-post)
+(aaw-initialize)
+(global-set-key (kbd "C-c w") #'aaw-new-post)
 
 ;; Store configuration created by custom in a separate file.
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
@@ -29,12 +30,13 @@
 (column-number-mode 1)
 (show-paren-mode)
 (global-hl-line-mode 1)
-(set-frame-font "DM Mono 11" nil t)
+(set-frame-font "Cascadia Code Light 11" nil t)
 (setq ring-bell-function 'ignore)
-(global-display-line-numbers-mode t)
 (setq confirm-kill-emacs #'yes-or-no-p)
+(winner-mode 1)
 
 ;; Don't make the screen jump when scroll off the top/bottom of the buffer.
+;; TODO figure out why/how this setting works.
 (setq scroll-conservatively 100)
 
 ;; Really, really, REALLY use UTF-8 everywhere.
@@ -44,33 +46,18 @@
 (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
+(defun s3thi/find-init-file ()
+  "Find init file."
+  (interactive)
+  (find-file user-init-file))
+
 ;; Some general keybindings.
 (global-set-key (kbd "C-x C-b") #'ibuffer)
 (global-set-key (kbd "M-;") #'comment-line)
 (global-set-key (kbd "M-o") #'other-window)
-
-(defun s3thi/split-window-right-and-follow ()
-  "Split window right and move cursor into the new window."
-  (interactive)
-  (split-window-right)
-  (other-window 1))
-
-(defun s3thi/split-window-below-and-follow ()
-  "Split window below and move cursor into the new window."
-  (interactive)
-  (split-window-below)
-  (other-window 1))
-
-(defun s3thi/find-user-init-file ()
-  "Open user's initialization file in the current window."
-  (interactive)
-  (find-file user-init-file))
-
-(global-set-key (kbd "C-0") #'delete-window)
-(global-set-key (kbd "C-1") #'delete-other-windows)
-(global-set-key (kbd "C-2") #'s3thi/split-window-below-and-follow)
-(global-set-key (kbd "C-3") #'s3thi/split-window-right-and-follow)
-(global-set-key (kbd "C-c I") #'s3thi/find-user-init-file)
+(global-set-key (kbd "<f12>") #'bookmark-bmenu-list)
+(global-set-key (kbd "C-<f12>") #'bookmark-set)
+(global-set-key (kbd "C-c i") #'s3thi/find-init-file)
 
 ;; Disable C-z to suspend in GUI Emacs.
 (when window-system
@@ -80,6 +67,12 @@
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 (put 'scroll-left 'disabled nil)
+
+;; Slightly better directory listing in dired.
+(setq dired-listing-switches "-lAhX --group-directories-first")
+
+;; Sentences end with a single space, not double space.
+(setq sentence-end-double-space nil)
 
 ;; Don't create garbage files.
 (setq-default create-lockfiles nil)
@@ -109,6 +102,8 @@
 (require 'package)
 (add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
 ;; Initialize use-package.
 (unless (package-installed-p 'use-package)
@@ -139,22 +134,18 @@
 
 ;; Hide certain modes from the modeline.
 (use-package diminish
-  :ensure t
-  :config
-  (diminish 'subword-mode))
+  :ensure t)
 
 ;; Gruvbox is timeless.
 (use-package gruvbox-theme
   :ensure t)
 
-;; The Spacemacs theme is polished af.
-(use-package spacemacs-theme
+;; Modus Operandi is a great light theme, but Modus Vivendi is a bit too harsh
+;; for my eyes.
+(use-package modus-themes
   :ensure t
-  :defer t
-  :init
-  (setq spacemacs-theme-org-height nil)
   :config
-  (load-theme 'spacemacs-light t))
+  (load-theme 'modus-operandi t))
 
 ;; Use Vertico for minibuffer completions.
 (use-package vertico
@@ -189,6 +180,10 @@
 (use-package magit
   :ensure t)
 
+(use-package forge
+  :ensure t
+  :after magit)
+
 ;; Highlight modified lines on the fly.
 (use-package diff-hl
   :ensure t
@@ -212,12 +207,15 @@
   :config
   (setq company-idle-delay 0.1)
   (setq company-tooltip-align-annotations t)
-  (setq company-selection-wrap-around t))
+  (setq company-selection-wrap-around t)
+  (setq company-dabbrev-downcase nil))
 
-(add-hook 'prog-mode-hook
-          (lambda ()
-            ;; Turn off soft wrapping for source code only.
-            (setq truncate-lines t)))
+(defun s3thi/prog-mode-hook ()
+  "Customizations for 'prog-mode' only."
+  (setq truncate-lines t) ;; Turn off soft wrapping.
+  (display-line-numbers-mode))
+
+(add-hook 'prog-mode-hook #'s3thi/prog-mode-hook)
 
 (global-subword-mode 1)
 
@@ -265,7 +263,9 @@
 ;; Syntax highlighting for Rust.
 (use-package rustic
   :ensure t
-  :diminish)
+  :diminish
+  :init
+  (setq rustic-format-on-save t))
 
 ;; Support for Go.
 (use-package go-mode
@@ -281,33 +281,8 @@
 (use-package lsp-mode
   :ensure t
   :init
-  (setq lsp-keymap-prefix "C-c l"))
-
-(defun s3thi/is-inside-frontmatter ()
-  "In a Markdown file, this returns t if the point is inside a
-frontmatter section"
-  (save-excursion
-    (let ((current-position (point))
-          (begin (point-min))
-          (end (progn (beginning-of-buffer)
-                      ;; TODO immediately return nil if either of these searches
-                      ;; fail.
-                      (search-forward-regexp "^---$" nil t)
-                      (search-forward-regexp "^---$" nil t)
-                      (point))))
-      (and (> current-position begin)
-           (< current-position end)))))
-
-(defun s3thi/markdown-auto-fill ()
-  "Set up auto-fill-mode for Markdown buffers"
-  (auto-fill-mode)
-  (setq auto-fill-function #'s3thi/markdown-auto-fill-function))
-
-(defun s3thi/markdown-auto-fill-function ()
-  "A custom auto-fill function for Markdown files that disables
-filling inside YAML frontmatter (if it exists)."
-  (unless (s3thi/is-inside-frontmatter)
-    (do-auto-fill)))
+  (setq lsp-keymap-prefix "C-c l")
+  (setq lsp-rust-analyzer-cargo-watch-command "clippy"))
 
 ;; Markdown.
 (use-package markdown-mode
@@ -316,7 +291,6 @@ filling inside YAML frontmatter (if it exists)."
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
-  :hook ((markdown-mode . s3thi/markdown-auto-fill))
   :init (setq markdown-command "multimarkdown"))
 
 (use-package rg
@@ -335,6 +309,10 @@ filling inside YAML frontmatter (if it exists)."
 (use-package expand-region
   :ensure t
   :bind (("C-=" . #'er/expand-region)))
+
+(use-package string-inflection
+  :ensure t
+  :bind (("C-c C-u" . #'string-inflection-all-cycle)))
 
 (use-package move-dup
   :ensure t
@@ -366,35 +344,31 @@ filling inside YAML frontmatter (if it exists)."
 
 (use-package org
   :bind (("C-c a" . org-agenda)
-         ("C-c c" . org-capture)
-
-         ;; Open the default org directory in dired.
-         ("<f12>" . (lambda ()
-                      (interactive)
-                      (dired org-directory))))
+         ("C-c c" . org-capture))
   :hook ((org-mode . auto-fill-mode))
   :config
   (setq org-directory "~/Dropbox/Org/")
-  (setq org-default-notes-file "~/Dropbox/Org/gtd/inbox.org")
+  (setq org-default-notes-file "~/Dropbox/Org/agenda/inbox.org")
   (add-to-list 'auto-mode-alist `("\\.org-archive$" . org-mode))
-  (setq org-agenda-files `("~/Dropbox/Org/gtd/inbox.org"
-                           "~/Dropbox/Org/gtd/inbox-mobile.org"
-                           "~/Dropbox/Org/gtd/tasks.org"
-                           "~/Dropbox/Org/gtd/projects.org"
-                           "~/Dropbox/Org/gtd/gluttony.org"))
+  (setq org-agenda-files `("~/Dropbox/Org/agenda/inbox.org"
+                           "~/Dropbox/Org/agenda/inbox-mobile.org"
+                           "~/Dropbox/Org/agenda/tasks.org"
+                           "~/Dropbox/Org/agenda/projects.org"
+                           "~/Dropbox/Org/agenda/gluttony.org"))
   (setq org-todo-keywords
-        '((sequence "TODO(t)" "NEXT(n)" "IN-PROGRESS(i)" "ONGOING(o)"
-                    "WAITING(w)" "SOMEDAY(s)" "|" "DONE(d)" "CANCELLED(c)")))
+        '((sequence "TODO(t)" "NEXT(n)" "IN-PROGRESS(i)" "WAITING(w)"
+                    "SOMEDAY(s)" "|"
+                    "DONE(d)" "CANCELLED(c)")))
   (setq org-todo-keyword-faces
         '(("TODO" . org-warning)
           ("NEXT" . "purple")
           ("IN-PROGRESS" . (:foreground "OliveDrab4" :weight bold))
-          ("ONGOING" . (:foreground "DarkCyan" :weight bold))
           ("WAITING" . org-warning)
           ("SOMEDAY" . "gray")
           ("DONE" . org-done)
           ("CANCELLED" .  org-done)))
   (setq org-hide-leading-stars t)
+  (setq org-hide-emphasis-markers t)
   (setq org-refile-targets '((nil :maxlevel . 3)
                              (org-agenda-files :maxlevel . 3)))
   (setq org-refile-use-outline-path t)
@@ -405,34 +379,63 @@ filling inside YAML frontmatter (if it exists)."
   (setq org-log-done t)
   (setq org-log-reschedule t)
   (setq org-capture-templates
-        '(("t" "todo" entry (file "~/Dropbox/Org/gtd/inbox.org")
+        '(("t" "todo" entry
+           (file "~/Dropbox/Org/agenda/inbox.org")
            "* TODO %?")
-          ("p" "project todo" entry (file+headline "~/Dropbox/Org/gtd/projects.org" "Refile")
+          ("p" "project todo" entry
+           (file+headline "~/Dropbox/Org/agenda/projects.org" "Inbox")
            "* TODO %?")
-          ("g" "gluttony" entry (file+headline "~/Dropbox/Org/gtd/gluttony.org" "Refile")
-           "* TODO %?")))
+          ("g" "gluttony" entry
+           (file+headline "~/Dropbox/Org/agenda/gluttony.org" "Inbox")
+           "* SOMEDAY %?")
+          ("i" "idea" entry
+           (file+headline "~/Dropbox/Org/notes/ideas.org" "Inbox")
+           "* SOMEDAY %?")))
   (setq org-agenda-custom-commands
         '(("y" "My agenda view"
-           ((agenda "")
+           ((agenda "" ((org-agenda-span 'day)))
             (todo "IN-PROGRESS" ((org-agenda-overriding-header "\nDoing now:")
                                  (org-agenda-files
-                                  '("~/Dropbox/Org/gtd/tasks.org"
-                                    "~/Dropbox/Org/gtd/gluttony.org"))))
+                                  '("~/Dropbox/Org/agenda/tasks.org"
+                                    "~/Dropbox/Org/agenda/gluttony.org"))))
             (todo "WAITING" ((org-agenda-overriding-header "\nWaiting on:")))
             (alltodo "" ((org-agenda-overriding-header "\nInboxes:")
                          (org-agenda-files
-                          '("~/Dropbox/Org/gtd/inbox.org"
-                            "~/Dropbox/Org/gtd/inbox-mobile.org"))))
+                          '("~/Dropbox/Org/agenda/inbox.org"
+                            "~/Dropbox/Org/agenda/inbox-mobile.org"))))
             (alltodo "" ((org-agenda-overriding-header "\nUnscheduled:")
                          (org-agenda-skip-function
                           '(org-agenda-skip-entry-if 'timestamp))
-                         (org-agenda-files '("~/Dropbox/Org/gtd/tasks.org")))))
+                         (org-agenda-files '("~/Dropbox/Org/agenda/tasks.org")))))
            ((org-agenda-block-separator nil)))
           ("o" "Things to do someday"
            ((todo "SOMEDAY"))
            ((org-agenda-overriding-header "Things to do someday:")
             (org-agenda-files
-             (remove "~/Dropbox/Org/gtd/projects.org" org-agenda-files)))))))
+             (remove "~/Dropbox/Org/agenda/projects.org" org-agenda-files))))))
+  (setq org-agenda-prefix-format
+        '((agenda . " %i %-16:c%?-12t% s")
+          (todo . " %i %-16:c")
+          (tags . " %i %-16:c")
+          (search . " %i %-16:c"))))
+
+(use-package org-journal
+  :ensure t
+  :init
+  (setq org-journal-prefix-key "C-c j")
+  (setq org-journal-file-type 'monthly)
+  (setq org-journal-file-format "journal-%Y-%m.org")
+  (setq org-journal-date-format "%A, %d %B %Y")
+  :config
+  (setq org-journal-dir "~/Dropbox/Org/journal/"))
+
+(use-package deft
+  :ensure t
+  :bind (("C-c l" . #'deft))
+  :init
+  (setq deft-extensions '("org"))
+  (setq deft-recursive t)
+  (setq deft-directory "~/Dropbox/Org/"))
 
 (use-package undo-tree
   :ensure t
@@ -442,12 +445,15 @@ filling inside YAML frontmatter (if it exists)."
   (setq undo-tree-visualizer-timestamps t)
   (setq undo-tree-visualizer-diff t))
 
-(use-package avy
-  :ensure t
-  :bind (("C-:" . #'avy-goto-char-2)))
+(use-package buffer-move
+  :ensure t)
 
-;; Start a server so other clients can connect to this.
-(server-start)
+(use-package browse-kill-ring
+  :ensure t)
+
+;; Start the Emacs server.
+(unless (server-running-p)
+  (server-start))
 
 (provide 'init)
 
