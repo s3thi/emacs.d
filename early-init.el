7e56a7f3c3b5ee201279bd679cb897a4953da6f4
Ankur Sethi <contact@ankursethi.com>
2025-10-19 00:56:20 +0530

    Changes, changes

--- early-init.el
+++ early-init.el
@@ -1,3 +1,11 @@
+;;; early-init.el --- Early init file for Emacs -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; This package sets up early initialization options for my Emacs
+;; config.
+
+;;; Code:
+
 (defun my-append-env-var (var-name value)
   "Append VALUE to the beginning of current value of env variable VAR-NAME."
   (setenv var-name (if (getenv var-name)
@@ -9,3 +17,7 @@
     (let ((gccjitpath "/opt/homebrew/lib/gcc/current:/opt/homebrew/lib"))
       (mapc (lambda (var-name) (my-append-env-var var-name gccjitpath))
             '("LIBRARY_PATH" "LD_LIBRARY_PATH" "PATH"))))
+
+(provide 'early-init)
+
+;;; early-init.el ends here
